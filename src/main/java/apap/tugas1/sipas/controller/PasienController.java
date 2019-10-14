02508c7266bc763acecacbdf7563f18bede038c1
addffa <adli.daffa5@gmail.com>
2019-10-14 16:41:49 +0700

    feat: create service for add new patient

--- src/main/java/apap/tugas1/sipas/controller/PasienController.java
+++ src/main/java/apap/tugas1/sipas/controller/PasienController.java
@@ -1,11 +1,12 @@
 package apap.tugas1.sipas.controller;
 
 import apap.tugas1.sipas.model.Pasien;
-import apap.tugas1.sipas.repository.PasienService;
+import apap.tugas1.sipas.service.PasienService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.List;
 
@@ -21,4 +22,17 @@ public class PasienController {
         model.addAttribute("pasienList", pasienList);
         return "beranda";
     }
+
+    @RequestMapping(value = "/pasien/tambah", method = RequestMethod.GET)
+    public String formTambahPasien(Model model) {
+        Pasien newPasien = new Pasien();
+        model.addAttribute(newPasien);
+        return "form-tambah-pasien";
+    }
+
+    @RequestMapping(value = "/pasien/tambah", method = RequestMethod.POST)
+    public String tambahPasien() {
+        return "tambah-pasien";
+    }
+
 }
--- src/main/java/apap/tugas1/sipas/service/PasienService.java
+++ src/main/java/apap/tugas1/sipas/service/PasienService.java
@@ -1,10 +1,13 @@
 package apap.tugas1.sipas.service;
 
 import apap.tugas1.sipas.model.Pasien;
+import apap.tugas1.sipas.model.PasienAsuransi;
 
 import java.util.List;
 
 public interface PasienService {
 
     List<Pasien> getPasienList();
+
+    void addPasien(Pasien pasien, PasienAsuransi pasienAsuransi);
 }
